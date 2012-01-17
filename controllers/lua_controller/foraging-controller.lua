d181616fddf36e720ed165f81405e50c2f9223f4
Matthias Hoelzl <tc@xantira.com>
2012-01-17 13:52:23 +0100

    We present: the first ARGoS controller written in Lua

--- controllers/lua_controller/foraging-controller.lua
+++ controllers/lua_controller/foraging-controller.lua
@@ -1,6 +1,99 @@
 -- foraging-controller.lua
 -- A controller that forages for food.
 
+-- SCEL
+-------
+
+initial_component = {
+   interface = {
+      get = function (self, pattern, location)
+	 local remote_component = self.interface:resolve_location(location)
+	 remote_component.interface:local_get(pattern, self)
+      end,
+      put = function (self, tuple, location)
+	 local remote_component = self.interface:resolve_location(location)
+	 remote_component.interface:local_put(tuple, self)
+      end,
+      qry = function (self, pattern, location)
+	 local remote_component = self.interface:resolve_location(location)
+	 remote_component.interface:local_qry(pattern, self)
+      end,
+      exec = function (process)
+	 -- add process to list of coroutines for self and yield
+      end,
+      new = function (interface, kb, policy, process)
+	 new_component = {
+	    interface = interface,
+	    knowledge_base = kb,
+	    policy = policy,
+	    process = process
+	 }
+	 return new_component
+      end,
+      resolve_location = function (location)
+	 return nil
+      end
+   },
+   knowledge_base = {},
+   policy = {
+      allow_put_access = function (self, tuple, sender)
+	 return true
+      end,
+      allow_get_access = function (self, tuple, sender)
+	 return true
+      end,
+      allow_qry_access = function (self, tuple, sender)
+	 return true
+      end
+   },
+   processes = {},
+   tuples = {}
+}
+
+function local_put (self, tuple, sender)
+   -- Put 'tuple' into the local tuple space if the policy allows it.
+   local tuples = self.tuples
+   local policy = self.policy
+   if (policy:allow_put_access(tuple, sender)) then
+      tuples[#tuples + 1] = tuple
+   end
+end
+
+function local_get (self, pattern, sender)
+   -- Get a tuple matching 'pattern' from the local tuple space, if
+   -- the policy agrees.
+   local tuples = self.tuples
+   local policy = self.policy
+   if (policy:allow_get_access(tuple, sender)) then
+      local index = match_tuple(pattern)
+      if index then
+	 return tuples[index]
+      else
+	 return false
+      end
+   end
+end
+
+function local_qry (self, pattern, sender)
+   -- Query the local tuple space for 'pattern' if the pollicy allows
+   -- it.
+   if (self.policy:allow_qry_access(tuple, sender)) then
+      local tuples = state.tuples
+      local index = match_tuple(pattern)
+      if index then
+	 return tuples[index]
+      else
+	 return false
+      end
+   end
+end
+
+function match_tuple (pattern)
+   -- Returns the index of a tuple matching pattern or false if no
+   -- such tuple exists in the tuple space
+   return false
+end
+
 -- Utility Functions.
 --------------------
 
@@ -20,7 +113,8 @@ end
 
 -- Each robot is running with its own Lua state, therefore we can just
 -- use a global variable to store the robot's state.
-state = {}
+state = {
+}
 
 function init (configuration_node, controller)
    state.uuid = argos_utils.uuid()
@@ -57,11 +151,11 @@ end
 function initialize_configuration_data (configuration_node)
    local log = argos_utils.log
    state.alpha = configuration_node:get_attribute("alpha") or 7.5
-   log("alpha = %s", state.alpha)
+   -- log("alpha = %s", state.alpha)
    state.delta = configuration_node:get_attribute("delta") or 0.1
-   log("delta = %s", state.delta)
+   -- log("delta = %s", state.delta)
    state.velocity = configuration_node:get_attribute("velocity") or 5
-   log("velocity = %s", state.velocity)
+   -- log("velocity = %s", state.velocity)
 end
 
 function control_step ()
@@ -69,12 +163,29 @@ function control_step ()
    log("Control step for robot %s", state.uuid)
    -- argos.print_module_types()
    local proximity_sensor = state.sensors.proximity
-   local proximity_readings = proximity_sensor:get_readings()
-   log("Proximity size: %s", tostring(proximity_readings:size()))
-   for i=1,proximity_readings:size() do
-      log("Value of proximity reading %u:\t%f",
-	  i,
-	  proximity_readings[i].value)
+   local readings = proximity_sensor:get_readings()
+   local size = readings:size()
+   local acc = argos.Vector2(0, 0)
+   for i = 0, size - 1 do
+      local reading = readings[i];
+      acc = acc + argos.Vector2(reading.value, reading.angle)
+   end
+   acc = acc / size
+   local angle = acc:angle()
+   -- TODO: remove inline constants
+   local wheel_actuator = state.actuators.wheels
+   local uuid = state.uuid
+   local v = state.velocity
+   if math.abs(angle:get_value()) < 0.1 and acc:length() < 0.1 then
+      log("%s: going straight ahead", uuid)
+      wheel_actuator:set_linear_velocity(v, v)
+   else
+      log("%s: turning", uuid)
+      if angle:get_value() < 0 then
+	 wheel_actuator:set_linear_velocity(0, v)
+      else
+	 wheel_actuator:set_linear_velocity(v, 0)
+      end
    end
 end
 
--- lua/argos-converters.cpp
+++ lua/argos-converters.cpp
@@ -3,17 +3,13 @@
 std::vector<ProximitySensorReading>
 to_proximity_sensor_reading_vector
 (std::vector<argos::CCI_FootBotProximitySensor::SReading> &readings) {
-  std::cerr << "In function to_proximity_sensor_reading_vector:" << std::endl;
   std::vector<ProximitySensorReading> result;
     // = new std::vector<ProximitySensorReading>();
   std::vector<argos::CCI_FootBotProximitySensor::SReading>::iterator it;
   for (it = readings.begin(); it < readings.end(); ++it) {
     argos::CCI_FootBotProximitySensor::SReading old_reading = *it;
     ProximitySensorReading new_reading(old_reading.Value, old_reading.Angle);
-    std::cerr << "Pushining new reading...";
     result.push_back(new_reading);
-    std::cerr << "done." << std::endl;
   }
-  std::cerr << "Done!" << std::endl;
   return result;
 }
